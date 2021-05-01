ae8e9c3229adf43939696ef27856868d9c475139
Andreas Brunner <abrunner@baart.ch>
2021-05-01 10:13:58 +0200

    preparing import works

---
+++ import/README.md
@@ -0,0 +1,1 @@
+Place your OsmAnd favourites.gpx here.
---
+++ prepareImport.php
@@ -0,0 +1,25 @@
+<?php
+
+// Configuration
+$importFile = '/v3/import/favourites.gpx';
+$wptType = 'white rabbit';
+
+// read the OsmAnd favourites
+$gpx = simplexml_load_file(dirname(__DIR__) . $importFile);
+
+// new xml object
+$newXML = new SimpleXMLElement("<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><gpx></gpx>");
+
+foreach ($gpx->wpt as $pt) {
+  if ($pt->type == $wptType) {
+
+    $wpt = $newXML->addChild('wpt');
+    $wpt->addAttribute('lat', (string) $pt['lat']);
+    $wpt->addAttribute('lon', (string) $pt['lon']);
+    $wpt->addChild('name', (string) $pt->name);
+    print((string) $pt->name . '<br>');
+  }
+}
+
+echo ('<pre>');
+var_dump($newXML->asXML('waypoints.gpx'));
